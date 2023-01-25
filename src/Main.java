c1b47b95648664794e03c45221f23b90f19a9455
Kseniya <okorokova.kseniya@gmail.com>
2023-01-25 12:55:29 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int price = 20_740;
+
+        int bonus = price / 20;
+        System.out.println("Бонус:" + bonus);
+    }
+
+}
